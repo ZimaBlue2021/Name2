c04ddb459d85c8ca54c15becee3fe3d483cfdb75
ZimaBlue2021 <67969113+ZimaBlue2021@users.noreply.github.com>
2020-07-28 08:18:43 -0700

    Add files via upload

---
+++ Naming.playground/Pages/12-ExerciseMorning.xcplaygroundpage/Contents.swift
@@ -0,0 +1,28 @@
+/*: 
+ ## Exercise: Fixing Your Morning
+ 
+ There’s a lot to get done before you leave home in the morning. This exercise will help you optimize your routine.
+ 
+ - callout(Exercise): Create a constant for each activity you do in the morning before leaving home: things like `brushTeeth`, `uploadPhotos`, `chooseClothes`, `shower`, `goJogging`, `finishHomework`, `fixLunch`, and so on. Think about how many minutes each activity usually takes, and assign that value to each constant.
+ */
+let brushTeeth = 5
+let chooseclothes = 5
+let makesureihavreverything = 5
+let usephoneifihavetime = 5
+let shower = 10
+let totaltimeneeded = shower + brushTeeth + chooseclothes
+
+
+
+var totaltime = shower + brushTeeth + chooseclothes + makesureihavreverything + usephoneifihavetime
+/*:
+ See what happens to your total time spent getting ready if you tweak the durations of the different activities. How short of a shower would you have to take in order to have more time to message your friends? Or go for a longer run? How much more time would you need if you decided to spend as long as you wanted doing all the activities you like best?
+ 
+ Change the numbers until you’ve got a design for your ideal morning. What would have to change in order for you to be able to actually spend your morning time this way?
+*/
+let Idealphoneususage = 90
+let Idealshowertime = 40
+
+
+let Idealmorning = Idealshowertime + Idealphoneususage + makesureihavreverything + chooseclothes + brushTeeth
+//:[Previous](@previous)  |  page 13 of 14  |  [Next: Exercise: Good Names](@next)
