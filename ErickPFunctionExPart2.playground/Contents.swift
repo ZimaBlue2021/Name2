b3ff4f1f70629d5c5e854a5f7534d03166bc8384
ZimaBlue2021 <67969113+ZimaBlue2021@users.noreply.github.com>
2020-07-29 14:40:53 -0700

    Add files via upload

---
+++ ErickPFunctionExPart2.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+struct carEngine {
+var v4engine = " a v4 engine has a four cylinder piston engine"
+var v6engine = " a v6 engine has six cylinder piston engine"
+var v8engine = " a v8 engine has eight cylinder piston engine"
+}
+let myCarEngine = carEngine()
+print("The differnce of each engine is \(carEngine()) I would recommended the v4 because it uses less fuel but if your a speed lover get the v8.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
