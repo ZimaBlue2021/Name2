d719653266f474291e0369ec5298a6f371b0b6d2
ZimaBlue2021 <67969113+ZimaBlue2021@users.noreply.github.com>
2020-07-28 19:14:14 -0700

    Ex

---
+++ FunctionExercise.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+func numbers(numOne: Int, numTwo: Int ) -> Int {
+var total = 0
+total = numOne + numTwo
+
+return total
+
+}
+
+var totalNumber = numbers(numOne: 4, numTwo: 4)
+print(numbers(numOne: 4, numTwo: 4))
+
+
+
+
+
+
+
+
+
+
+
