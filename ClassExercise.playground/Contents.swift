895a186a381a036daa037133814ebff8365cf8c9
ZimaBlue2021 <67969113+ZimaBlue2021@users.noreply.github.com>
2020-07-28 19:37:07 -0700

    Add files via upload

---
+++ ClassExercise.playground/Contents.swift
@@ -0,0 +1,58 @@
+import UIKit
+
+class carPreference {
+    
+    enum Seats {
+        case leather
+        case Nylon
+        case polyester
+    }
+}
+    enum wheelDrive {
+case twoWheelDrive
+case fourWheelDrive
+}
+enum color {
+    case silver
+    case black
+    case req
+    case blue
+    case white
+}
+    enum engine {
+        case v4
+        case v8
+}
+print("My prefernce for a car are leather seats, \(wheelDrive.fourWheelDrive), car color \(color.black), and a \(engine.v4) engine. ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
