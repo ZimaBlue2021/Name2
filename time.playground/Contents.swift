d8e1fb931669825b5a3e73e055adea9b04c2a28d
ZimaBlue2021 <67969113+ZimaBlue2021@users.noreply.github.com>
2020-07-28 19:26:28 -0700

    Add files via upload

---
+++ time.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+var Time = 180
+
+
+for index in 1...180 {
+print(" seconds \(index)")
+}
+
+if Time <= 180 {
+print(" wait ")
+}
+if Time == 180 {
+print(" Ready ")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
