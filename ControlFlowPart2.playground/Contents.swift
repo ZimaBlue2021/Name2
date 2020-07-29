d8039b4d9edda80fa8b959b22dc743dc8da2cb0f
ZimaBlue2021 <67969113+ZimaBlue2021@users.noreply.github.com>
2020-07-29 14:52:03 -0700

    Add files via upload

---
+++ ControlFlowPart2.playground/Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+
+var itemscollected: [String] = ["bass guitar", "xbox"]
+
+itemscollected.append ("switch")
+
+itemscollected += ["electric guitar", "acoustic guitar"]
+
+print("my aray has \(itemscollected.count) items")
+
+itemscollected
+
+var firstitem = itemscollected[0]
+
+for item in itemscollected.sorted()  {
+
+print(item)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
