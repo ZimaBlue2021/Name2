5805775f0eff14540032171ab40f8556b087b08d
ZimaBlue2021 <67969113+ZimaBlue2021@users.noreply.github.com>
2020-08-06 08:41:50 -0700

    Add files via upload

---
+++ Erick.P(TabbedAppEx1st)/Erick.P(TabbedAppEx1st)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Erick.P(TabbedAppEx1st)
+//
+//  Created by Erick P on 8/6/20.
+//  Copyright © 2020 Erick P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
